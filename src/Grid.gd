8d3d5cf85cb01cb320d1d41399d54782e542f040
andrew-wilkes <github@urgentclick.com>
2022-03-02 17:19:44 +0900

    Refactor grid drawing code

--- src/Grid.gd
+++ src/Grid.gd
@@ -20,28 +20,18 @@ func _draw():
 	var grid_size = rect_size
 	var xstep = grid_size.x + 4
 	var ystep = grid_size.y + 4
-	var x = -2
-	var y = -2
-	for n in 2:
-		draw_line(Vector2(x, -offset), Vector2(x, grid_size.y + offset), line_color, 4)
-		draw_line(Vector2(-offset, y), Vector2(grid_size.x + offset, y), line_color, 4)
-		x += xstep
-		y += ystep	
+	draw_lines(-2, -2, xstep, ystep, 2, offset, grid_size, line_color, 4)
 	xstep = grid_size.x / 3
 	ystep = grid_size.y / 3
-	x = xstep
-	y = ystep
-	for n in 2:
-		draw_line(Vector2(x, -offset), Vector2(x, grid_size.y + offset), line_color, 2)
-		draw_line(Vector2(-offset, y), Vector2(grid_size.x + offset, y), line_color, 2)
-		x += xstep
-		y += ystep
+	draw_lines(xstep, ystep, xstep, ystep, 2, offset, grid_size, line_color, 2)
 	xstep = grid_size.x / 9
 	ystep = grid_size.y / 9
-	x = xstep
-	y = ystep
-	for n in 8:
-		draw_line(Vector2(x, -offset), Vector2(x, grid_size.y + offset), line_color, 1)
-		draw_line(Vector2(-offset, y), Vector2(grid_size.x + offset, y), line_color, 1)
+	draw_lines(xstep, ystep, xstep, ystep, 8, offset, grid_size, line_color, 1)
+
+
+func draw_lines(x, y, xstep, ystep, num_reps, offset, grid_size, line_color, line_thickness):
+	for n in num_reps:
+		draw_line(Vector2(x, -offset), Vector2(x, grid_size.y + offset), line_color, line_thickness)
+		draw_line(Vector2(-offset, y), Vector2(grid_size.x + offset, y), line_color, line_thickness)
 		x += xstep
-		y += ystep
+		y += ystep	
